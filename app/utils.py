23f2f7334678db517d208c9504fd628c75aa97bd
Saumyajeet Varma <saumyajeetv@gmail.com>
2026-03-03 17:27:05 +0530

    basic utils defined

---
+++ app/utils.py
@@ -0,0 +1,14 @@
+import requests
+import re
+import pandas as pd
+from datetime import timezone
+from dateutil.parser import parse
+
+def fetch_logs(url):
+    pass
+
+def sanitize_filename(url):
+    pass
+
+def format_export_data(df, fmt):
+    pass
